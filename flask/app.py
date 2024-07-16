547b8fdefd82effbd3f3aabfe4262e1537af37a1
breadpandedpan <krisha.agarwal@csedge.org>
2024-07-16 14:36:43 -0700

    added animation & made it root pg

--- flask/app.py
+++ flask/app.py
@@ -3,13 +3,19 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/eightball', methods=['GET','POST'])
+# @app.route('/')
+# def homepage():
+#     return render_template('homepage.html')
+
+#ask.com
+
+@app.route('/', methods=['GET','POST'])
 def eightball():
     if request.method == 'POST':
         answers = ["I don't know", "/j", "hell no", "maybe", "no", "yeah, for sure"]
-        return f'The eightball says {random.choice(answers)}!'
-    return render_template('eightball.html', question=url_for('eightball'))
-
+        return f"<h1> The eightball says {random.choice(answers)}! </h1>"
+    return render_template('index.html', question=url_for('eightball'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=70, debug=True)
+
